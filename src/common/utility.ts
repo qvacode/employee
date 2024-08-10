ef5cfcad85280d98d6d43d4fee8a3d6436ec36f2
David Ernesto <dhernandezt9303@gmail.com>
2024-08-10 12:34:21 -0400

    feat: agregamos librerías de validación de datos y swagger, scripts y creamos nuestra clase de utilidad

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -1,7 +1,18 @@
 import { v4 as uuid } from "uuid";
+import { compare, hash } from "bcryptjs";
 
 export class Utility {
+    
     public static generateId(): string {
-        return uuid()
+        return uuid();
+    }
+    
+    public static async hashPassword(password: string): Promise<string> {
+        const saltRounds = 10;
+        return await hash(password, saltRounds);
+    }
+
+    public static async validatePassword(password: string, hash: string): Promise<boolean> {
+        return await compare(password, hash);
     }
 }
