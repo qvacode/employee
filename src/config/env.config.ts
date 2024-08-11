cb85ada40782c1d334ed7732c3e3b8640693f329
David Ernesto <dhernandezt9303@gmail.com>
2024-08-11 16:04:56 -0400

    feat: agregamos validación de variables de entorno

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,30 @@
 import 'dotenv/config';
+import Joi from 'joi';
+
+interface EnvironmentVars {
+  PORT: number
+  JWT_SECRET: string
+  JWT_EXPIRATION: string
+  MONGO_URL: string
+}
+
+const environmentSchema = Joi.object<EnvironmentVars>({
+    PORT: Joi.number().required(),
+    JWT_SECRET: Joi.string().required(),
+	JWT_EXPIRATION: Joi.string().required(),
+	MONGO_URL: Joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = environmentSchema.validate(process.env)
+
+if(error) throw new Error(`Config validation errors: ${error.message}`)
+
+const environment: EnvironmentVars = value
 
 export const Env = {
-	PORT: 4003,
-	HOST: <string>process.env.HOST,
-};
+	PORT: environment.PORT,
+	JWT_SECRET: environment.JWT_SECRET,
+	JWT_EXPIRATION: environment.JWT_EXPIRATION,
+	MONGO_URL: environment.MONGO_URL
+}
