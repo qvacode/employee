14ad0d0a460f1d8fa36b64ecd319bd76c87dc9da
David Ernesto <dhernandezt9303@gmail.com>
2024-08-11 16:10:57 -0400

    feat: creamos capa de aplicación para modulo de preguntas

---
+++ src/modules/questions/application/question.use-cases.ts
@@ -0,0 +1,39 @@
+import { DomainExceptions } from "../../../common/domain/exceptions";
+import { CreateQuestionDto, Question, QuestionPrimitiveData } from "../domain/question.entity";
+import { QuestionRepository } from "../domain/question.repository";
+
+export class QuestionUseCases {
+    constructor(private readonly questionRepository: QuestionRepository) {}
+
+    public create = async (question: CreateQuestionDto): Promise<QuestionPrimitiveData> => {
+        const newQuestion = Question.create(question)
+        await this.questionRepository.create(newQuestion)
+
+        return newQuestion.toValue()
+    }
+
+    public findById = async (id: string): Promise<QuestionPrimitiveData> => {
+        const question = await this.questionRepository.findById(id)
+        if(!question) throw new DomainExceptions(`Question with id = ${id} not found!`, 404)
+
+        return question
+    }
+
+    public findAll = async (): Promise<QuestionPrimitiveData[]> => {
+        return await this.questionRepository.findAll()
+    }
+
+    public answer = async (id: string, answer: string): Promise<QuestionPrimitiveData> => {
+        const question = await this.questionRepository.answer(id, answer)
+        if(!question) throw new DomainExceptions(`Question with id = ${id} not found!`, 404)
+
+        return question
+    }
+
+    public qualify = async (id: string, score: number): Promise<QuestionPrimitiveData> => {
+        const question = await this.questionRepository.qualify(id, score)
+        if(!question) throw new DomainExceptions(`Question with id = ${id} not found!`, 404)
+
+        return question
+    }
+}
