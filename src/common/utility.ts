36f2cfe9703d2a404242c548a1e7e69ef3e4b7c6
David Ernesto <dhernandezt9303@gmail.com>
2024-08-11 16:03:47 -0400

    feat: agregamos generación y validación de json web token

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -1,7 +1,16 @@
 import { v4 as uuid } from "uuid";
 import { compare, hash } from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import { Env } from "../config/env.config";
+import { DomainExceptions } from "./domain/exceptions";
+import { UserRole } from "../modules/users/domain/user.entity";
+
+export type Payload = jwt.JwtPayload
 
 export class Utility {
+    private static readonly JWT_SECRET = Env.JWT_SECRET
+    private static readonly JWT_EXPIRATION = Env.JWT_EXPIRATION
     
     public static generateId(): string {
         return uuid();
@@ -14,5 +23,19 @@ export class Utility {
 
     public static async validatePassword(password: string, hash: string): Promise<boolean> {
         return await compare(password, hash);
+    }
+
+    public static generateToken(userId: string, role: UserRole): string {
+        return jwt.sign({ id: userId, role }, this.JWT_SECRET, {
+            expiresIn: this.JWT_EXPIRATION,
+        });
+    }
+
+    public static validateToken(token: string): string | object {
+        try {
+            return jwt.verify(token, this.JWT_SECRET);
+        } catch (error) {
+            throw new DomainExceptions('Invalid or expired token', 401);
+        }
     }
 }
