401b66cf37c955924aff04ba88fd533f5f52b3ed
David Ernesto <dhernandezt9303@gmail.com>
2024-08-11 16:04:29 -0400

    feat: agregamos rate limit

--- src/common/infrastructure/app.middleware.ts
+++ src/common/infrastructure/app.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { DomainExceptions } from '../domain/exceptions';
 import { ValidationError } from 'joi';
+import rateLimit from 'express-rate-limit';
 
 export function errorHandler(
     error: any,
@@ -20,8 +21,22 @@ export function errorHandler(
     }
 }
 
-export const asyncHandler =
-    (fn: RequestHandler) =>
-    (req: Request, res: Response, next: NextFunction) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
+export const asyncHandler = (fn: RequestHandler) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+export const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+    message: {
+        status: 429,
+        error: 'Too many requests, please try again later.'
+    },
+    handler: (req, res, next, options) => {
+        res.status(options.message.status).json({
+            message: options.message.error,
+        });
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
